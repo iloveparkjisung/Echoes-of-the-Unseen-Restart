f644c1864ae4fc66fd7bbb3ea7a7b8de324e1240
Aryanne <aryannemondeja@riccarton.school.nz>
2026-05-27 10:23:35 +1200

    Imported my  player placeholder
    so i could try out the screen to see if the collisions work .

---
+++ scripts/fp_cam.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+var sensiivity = 0.2
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensiivity))
+		rotate_x(deg_to_rad(-event.relative.y * sensiivity))
+		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
