522fbe2ec22017d5f271bcb060b9dd5f7181f7e7
Aryanne <aryannemondeja@riccarton.school.nz>
2026-06-03 10:15:23 +1200

    I have gotten and updated my scripts
    I used the same person's tutorial but for cameras

--- scripts/fp_cam.gd
+++ scripts/fp_cam.gd
@@ -1,9 +1,42 @@
-extends Node3D
+extends CharacterBody3D
 
-var sensiivity = 0.2
+@onready var camera_3d = $Camera3D
 
-func _input(event: InputEvent) -> void:
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const CAMERA_SENS = 0.003
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): get_tree().quit()
+	
 	if event is InputEventMouseMotion:
-		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensiivity))
-		rotate_x(deg_to_rad(-event.relative.y * sensiivity))
-		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		rotation.y -= event.relative.x * CAMERA_SENS
+		rotation.x -= event.relative.y * CAMERA_SENS
+		rotation.x = clamp(rotation.x, -0.5, 1.2)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
