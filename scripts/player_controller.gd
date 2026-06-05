cc59ab7624fddfa769c22c3cd940e16334a18e00
Aryanne <aryannemondeja@riccarton.school.nz>
2026-06-05 13:04:07 +1200

    making my character move
    this is currently not working but this is what I have currently

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,10 +1,26 @@
 class_name PlayerController extends CharacterBody3D
 
+var _input_dir : Vector2 = Vector2.ZERO
+var _movement_velocity : Vector3 = Vector3.ZERO
+var speed : float = 0.0
+@export var acceleration : float = 0.1
+@export var deceleration : float = 0.2
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	_input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var current_velocity = Vector2(_movement_velocity.x, _movement_velocity.z)
+	var direction = (transform.basis * Vector3(_input_dir.x, 0, _input_dir.y).normalized())
+	
+	if direction:
+		current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * speed, acceleration)
+	else:
+		current_velocity = current_velocity.move_toward(Vector2.ZERO, deceleration)
+	
+	_movement_velocity = Vector3(current_velocity.x, velocity.y, current_velocity.y)
+	velocity = _movement_velocity	
 	
 	move_and_slide()
 
