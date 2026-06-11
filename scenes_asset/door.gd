e19e3e08586e28966a077069e1f62b51c3a0ef1f
Aryanne <aryannemondeja@riccarton.school.nz>
2026-06-12 11:44:41 +1200

    fixing code for door

--- scenes_asset/door.gd
+++ scenes_asset/door.gd
@@ -1,10 +1,10 @@
 extends Area3D
 @export_file("res://Echoes-of-the-Unseen-Restart/scenes_asset/buildinginside.tscn") var target_scene: String
-
+@onready var interaction_label: Label = $CanvasLayer/Label
 var is_player_in_door: bool = false
 
 func _ready() -> void:
-	body_entered.connect(on_body_entered)
+	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -18,6 +18,6 @@ func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		is_player_in_door = false
 
-func _on_bosy_exited(body: Node3D) -> void:
+func _on_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		is_player_in_door = false
