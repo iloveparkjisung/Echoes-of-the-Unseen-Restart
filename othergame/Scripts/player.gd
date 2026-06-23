e119ee02a04c62edb62ae17a4212db4a63bdab9a
Aryanne <aryannemondeja@riccarton.school.nz>
2026-06-24 10:30:11 +1200

    I am still trying to figure out my mouse situation

--- othergame/Scripts/player.gd
+++ othergame/Scripts/player.gd
@@ -44,7 +44,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func move():
-	var input_dir = Input.get_vector("A", "D", "W", "S")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	if direction:
--- scripts/fp_cam.gd
+++ scripts/fp_cam.gd
@@ -1,10 +1,12 @@
-extends Camera3D
+extends CharacterBody3D
 
+# This gets the child Camera3D node for vertical looking
 @onready var camera_3d = $Camera3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const CAMERA_SENS = 0.003
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -12,12 +14,16 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _input(event):
-	if event.is_action_pressed("ui_cancel"): get_tree().quit()
+	if event.is_action_pressed("ui_cancel"): 
+		get_tree().quit()
 	
 	if event is InputEventMouseMotion:
+		# Rotate the entire body left/right
 		rotation.y -= event.relative.x * CAMERA_SENS
-		rotation.x -= event.relative.y * CAMERA_SENS
-		rotation.x = clamp(rotation.x, -0.5, 1.2)
+		
+		# Rotate only the camera up/down
+		camera_3d.rotation.x -= event.relative.y * CAMERA_SENS
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, -1.2, 1.2)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -28,10 +34,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction based on the body's transform
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
